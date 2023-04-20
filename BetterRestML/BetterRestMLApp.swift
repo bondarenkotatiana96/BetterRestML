a108a83ed406d87d08e7d4fd52f72964228747b2
Tatiana Bondarenko <62190724+bondarenkotatiana96@users.noreply.github.com>
2023-04-19 17:20:05 -0700

    Add files via upload

---
+++ BetterRestML/BetterRestMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestMLApp.swift
+//  BetterRestML
+//
+//  Created by Tatiana Bondarenko on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
